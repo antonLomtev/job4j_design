842e0279946c44ca6440798a41fe259e87d95f16
a.stogniy <a.stogniy@astondevs.ru>
2023-12-11 17:23:51 +0300

    1. Создать элементарную структуру дерева [#1711]

---
+++ src/test/java/ru/job4j/tree/SimpleTreeTest.java
@@ -0,0 +1,54 @@
+package ru.job4j.tree;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class SimpleTreeTest {
+    @Test
+    void when6ElFindLastThen6() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+        tree.add(4, 5);
+        tree.add(5, 6);
+        assertThat(tree.findBy(6)).isPresent();
+    }
+
+    @Test
+    void whenElFindNotExistThenOptionEmpty() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        assertThat(tree.findBy(7)).isEmpty();
+    }
+
+    @Test
+    void whenChildExistOnLeafThenNotAdd() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+        tree.add(4, 5);
+        tree.add(5, 6);
+        assertThat(tree.add(2, 6)).isFalse();
+    }
+
+    @Test
+    void whenOptionalIsEmpty() {
+        Tree<Integer> simpleTree = new SimpleTree<>(1);
+        simpleTree.add(1, 4);
+        assertThatThrownBy(() -> simpleTree.findBy(5).orElseThrow())
+                .isInstanceOf(NoSuchElementException.class);
+    }
+
+    @Test
+    void whenAddIsTrue() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        assertThat(tree.add(1, 2)).isTrue();
+        assertThat(tree.findBy(2)).isPresent();
+    }
+}
