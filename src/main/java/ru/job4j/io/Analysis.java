64cae8c1eb1dc2e9c06d1980bd24cbb369a1b159
a.stogniy <a.stogniy@astondevs.ru>
2023-12-25 00:37:05 +0300

    7. Анализ доступности сервера [#859]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -1,7 +1,28 @@
 package ru.job4j.io;
 
+import java.io.*;
+
 public class Analysis {
     public void unavailable(String source, String target) {
+        try (BufferedReader in = new BufferedReader(new FileReader(source));
+             BufferedWriter out = new BufferedWriter(new FileWriter(target))) {
+            String start = "";
+            for (String line = in.readLine(); line != null; line = in.readLine()) {
+                String[] status = line.split(" ");
+                if (Integer.valueOf(status[0]).equals(400) || Integer.valueOf(status[0]).equals(500)) {
+                    if (start.isEmpty()) {
+                        start = status[1];
+                    }
+                } else {
+                    if (!start.isEmpty()) {
+                        out.write(start + "; " + status[1] + ";" + "\n");
+                        start = "";
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
