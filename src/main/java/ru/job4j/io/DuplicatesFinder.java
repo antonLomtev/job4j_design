f0f5eb28bd01f75928341f92825200e4849e3ed6
a.stogniy <a.stogniy@astondevs.ru>
2023-12-26 23:32:11 +0300

    11. Поиск дубликатов [#315066]

---
+++ src/main/java/ru/job4j/io/DuplicatesFinder.java
@@ -0,0 +1,20 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+
+public class DuplicatesFinder {
+    public static void main(String[] args) throws IOException {
+        searchDuplicates(Paths.get(".")).forEach((key, value) ->  System.out.println(key.getName().equals("404.txt") ? value : " "));
+    }
+
+    public static Map<FileProperty, List<String>> searchDuplicates(Path path) throws IOException {
+        DuplicatesVisitor duplicatesVisitor = new DuplicatesVisitor();
+        Files.walkFileTree(path, duplicatesVisitor);
+        return duplicatesVisitor.getData();
+    }
+}
---
+++ src/main/java/ru/job4j/io/DuplicatesVisitor.java
@@ -0,0 +1,35 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
+    private Map<FileProperty, List<String>> data = new HashMap<>();
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        FileProperty fileProperty = new FileProperty(Files.size(file), file.getFileName().toString());
+        if (data.containsKey(fileProperty)) {
+            ArrayList<String> arrayList = new ArrayList<>(data.get(fileProperty));
+            arrayList.add(file.toAbsolutePath().toString());
+            data.put(fileProperty, arrayList);
+        } else {
+            ArrayList<String> temp = new ArrayList<>();
+            temp.add(file.toAbsolutePath().toString());
+            data.put(fileProperty, temp);
+        }
+        return super.visitFile(file, attrs);
+    }
+
+    public Map<FileProperty, List<String>> getData() {
+        return data;
+    }
+}
