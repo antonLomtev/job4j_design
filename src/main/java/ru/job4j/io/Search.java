489d3abc9c16e3643e3e1fb08fea9055aa237628
a.stogniy <a.stogniy@astondevs.ru>
2023-12-27 21:50:55 +0300

    12. Валидация параметров запуска [#246865 #469072]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,10 +9,9 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        if (validation(args)) {
-            Path start = Paths.get(args[0]);
-            search(start, path -> path.toFile().getName().endsWith(args[1])).forEach(System.out::println);
-        }
+        validation(args);
+        Path start = Paths.get(args[0]);
+        search(start, path -> path.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
@@ -21,10 +20,15 @@ public class Search {
         return searchFiles.getPaths();
     }
 
-    private static boolean validation(String[] args) {
-        if (args.length == 0 || !args[0].startsWith("./") || !args[1].startsWith(".")) {
-            throw new IllegalArgumentException("Wrong format args");
+    private static void validation(String[] args) {
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Fill in all parameters.");
         }
-        return true;
+        if (!Files.exists(Paths.get(args[0]))) {
+            throw new IllegalArgumentException("Directory/File not found.");
+        }
+        if (!args[1].startsWith(".") && args[1].length() < 2) {
+            throw new IllegalArgumentException("Incorrect format file.");
+        }
     }
 }
