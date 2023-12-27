bc93f6b262eee3f7b318a898b23c2bf2015f6512
a.stogniy <a.stogniy@astondevs.ru>
2023-12-27 18:59:58 +0300

    12. Валидация параметров запуска [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,13 +9,22 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, path -> path.toFile().getName().endsWith(".txt")).forEach(System.out::println);
+        if (validation(args)) {
+            Path start = Paths.get(args[0]);
+            search(start, path -> path.toFile().getName().endsWith(args[1])).forEach(System.out::println);
+        }
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searchFiles = new SearchFiles(condition);
         Files.walkFileTree(root, searchFiles);
         return searchFiles.getPaths();
+    }
+
+    private static boolean validation(String[] args) {
+        if (args.length == 0 || !args[0].startsWith("./") || !args[1].startsWith(".")) {
+            throw new IllegalArgumentException("Wrong format args");
+        }
+        return true;
     }
 }
