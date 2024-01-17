7c17abd3f5717d4fed38fcb28edc3ea3f06b6e4d
= <a.stogniy@astondevs.ru>
2024-01-17 10:52:39 +0300

    3. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -16,8 +16,13 @@ public class EchoServer {
                      BufferedReader input = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    if (input.readLine().contains("msg=Bye")) {
+                    String message = input.readLine();
+                    if (message.contains("msg=Exit")) {
                         server.close();
+                    } else if (message.contains("msg=Hello")) {
+                        out.write("Hello".getBytes());
+                    } else {
+                        out.write("What?".getBytes());
                     }
                     out.flush();
                 }
