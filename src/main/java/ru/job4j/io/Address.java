0da0d1903c44a9892b3b8452e1a758e24cea44ad
a.stogniy <a.stogniy@astondevs.ru>
2024-01-18 22:10:28 +0300

    5. Преобразование JSON в POJO. JsonObject [#315064]

--- src/main/java/ru/job4j/io/Address.java
+++ src/main/java/ru/job4j/io/Address.java
@@ -1,7 +1,12 @@
 package ru.job4j.io;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
+import java.util.ArrayList;
+import java.util.List;
 
 @XmlRootElement(name = "address")
 public class Address {
@@ -19,10 +24,36 @@ public class Address {
         this.number = number;
     }
 
+    public String getStreet() {
+        return street;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
     @Override
     public String toString() {
         return "Address{"
                 + "street='" + street + '\''
                 + ", number=" + number + '}';
+    }
+
+    public static void main(String[] args) {
+        JSONObject jsonAddress = new JSONObject("{\"street\": \"Kosmonavtov\","
+                                                     + "\"number\": 50}");
+        JSONArray jsonArray1 = new JSONArray(new Integer[]{1, 2, 3, 4, 5});
+        final House house = new House(true, 1992, "Stabilnost",
+                new Integer[]{1, 2, 3, 4, 5}, new Address("Kosmonavtov", 50));
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("isBrick", house.isBrick());
+        jsonObject.put("yearOfConstruction", house.getYearOfConstruction());
+        jsonObject.put("company", house.getCompany());
+        jsonObject.put("entrance", jsonArray1);
+        jsonObject.put("address", jsonAddress);
+
+        System.out.println(jsonObject.toString());
+
+        System.out.println(new JSONObject(house).toString());
     }
 }
