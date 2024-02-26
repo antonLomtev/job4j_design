698da5191aa5fd99403304a46df093817c5e9734
= <a.stogniy@astondevs.ru>
2024-02-26 23:57:11 +0300

    4. Найти утечку памяти. [#504882]

---
+++ src/main/java/ru/job4j/gc/leak/UserGenerator.java
@@ -0,0 +1,57 @@
+package ru.job4j.gc.leak;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class UserGenerator implements Generate {
+    public final String pathNames = "src/main/java/ru/job4j/gc/leak/files/names.txt";
+    public final String pathSurnames = "src/main/java/ru/job4j/gc/leak/files/surnames.txt";
+    public final String pathPatrons = "src/main/java/ru/job4j/gc/leak/files/patr.txt";
+
+    public final String separator = " ";
+    public final int newUsers = 1000;
+
+    public List<String> names;
+    public List<String> surnames;
+    public List<String> patrons;
+    private List<User> users = new ArrayList<>();
+    private Random random;
+
+    public UserGenerator(Random random) {
+        this.random = random;
+        readAll();
+    }
+
+    private void readAll() {
+        try {
+            names = read(pathNames);
+            surnames = read(pathSurnames);
+            patrons = read(pathPatrons);
+        } catch (IOException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    public User randomUser() {
+        return users.get(random.nextInt(users.size()));
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    @Override
+    public void generate() {
+        users.clear();
+        for (int i = 0; i < newUsers; i++) {
+            String user = String.format("%s%s%s%s%s",
+                    surnames.get(random.nextInt(surnames.size())), separator,
+                    names.get(random.nextInt(names.size())), separator,
+                    patrons.get(random.nextInt(patrons.size()))
+            );
+            users.add(new User(user));
+        }
+    }
+}
