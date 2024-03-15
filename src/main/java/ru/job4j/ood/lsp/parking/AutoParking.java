e733fb69d52dd3f9714c4c8ad068ef0e3d65197b
a.stogniy <a.stogniy@astondevs.ru>
2024-03-15 23:40:33 +0300

    2. Парковка машин [#853] Impl

--- src/main/java/ru/job4j/ood/lsp/parking/AutoParking.java
+++ src/main/java/ru/job4j/ood/lsp/parking/AutoParking.java
@@ -1,16 +1,23 @@
 package ru.job4j.ood.lsp.parking;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class AutoParking implements Parking {
     private int countParkingSpacesCar;
     private int countParkingSpacesTruck;
+    private int sizeCarParking;
+    private int sizeTruckParking;
     private List<Auto> carParking;
     private List<Auto> truckParking;
 
     public AutoParking(int countParkingSpacesCar, int countParkingSpacesTruck) {
         this.countParkingSpacesCar = countParkingSpacesCar;
         this.countParkingSpacesTruck = countParkingSpacesTruck;
+        carParking = new ArrayList<>();
+        truckParking = new ArrayList<>();
+        sizeCarParking = 0;
+        sizeTruckParking = 0;
     }
 
     public int getCountParkingSpacesCar() {
@@ -23,16 +30,54 @@ public class AutoParking implements Parking {
 
     @Override
     public boolean freeParkingSpaces() {
-        return carParking.size() + truckParking.size() < countParkingSpacesCar + countParkingSpacesTruck;
+        return sizeCarParking + sizeTruckParking < countParkingSpacesCar + countParkingSpacesTruck;
     }
 
     @Override
     public void addAuto(Auto auto) {
+        if (freeParkingSpaces()) {
+            if (auto.getSize() == 1 && carParking.size() < countParkingSpacesCar) {
+                carParking.add(auto);
+                sizeCarParking++;
+            } else if (auto.getSize() > 1 && sizeTruckParking + auto.getSize() <= countParkingSpacesTruck) {
+                truckParking.add(auto);
+                sizeTruckParking += auto.getSize();
+            } else if (auto.getSize() > 1 && sizeCarParking + auto.getSize() <= countParkingSpacesCar) {
+                carParking.add(auto);
+                sizeCarParking += auto.getSize();
+            }
+        }
+    }
 
+    public int getSizeCarParking() {
+        return sizeCarParking;
+    }
+
+    public int getSizeTruckParking() {
+        return sizeTruckParking;
+    }
+
+    public List<Auto> getCarParking() {
+        return carParking;
+    }
+
+    public List<Auto> getTruckParking() {
+        return truckParking;
     }
 
     @Override
     public List<Auto> findAll() {
-        return null;
+        List<Auto> autos = new ArrayList<>();
+        if (!carParking.isEmpty()) {
+            for (Auto a : carParking) {
+                autos.add(a);
+            }
+        }
+        if (!truckParking.isEmpty()) {
+            for (Auto a : truckParking) {
+                autos.add(a);
+            }
+        }
+        return autos;
     }
 }
