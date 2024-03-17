fcb4c3da54a6c0105123508420e7ad81f0ac7f4d
= <a.stogniy@astondevs.ru>
2024-03-17 21:19:01 +0300

    чистка

--- src/main/java/ru/job4j/ood/lsp/parking/Car.java
+++
@@ -1,7 +0,0 @@
-package ru.job4j.ood.lsp.parking;
-
-public class Car extends Auto {
-    public Car(int size) {
-        super(size);
-    }
-}
--- src/main/java/ru/job4j/ood/lsp/parking/Truck.java
+++
@@ -1,7 +0,0 @@
-package ru.job4j.ood.lsp.parking;
-
-public class Truck extends Auto {
-    public Truck(int size) {
-        super(size);
-    }
-}
